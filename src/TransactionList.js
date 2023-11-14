3a4fae037bf823c084aad8ea35c59593f19b0381
dian <dianjeruto@gmail.com>
2023-11-14 22:28:00 +0300

    first commit

---
+++ src/TransactionList.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import TransactionItem from './TransactionItem';
+import SearchTransaction from './SearchTransaction';
+import AddTransactionForm from './AddTransactionForm';
+
+function TransactionList(){
+  const [transactions, setTransactions] = useState([]);
+  const [searchParam, setSearchParam] = useState("");
+
+  //GET TRANSACTIONS FROM DB
+  useEffect(() => {
+    fetch(`http://localhost:3000/transactions`)
+      .then((res) => res.json())
+      .then((transactions) => setTransactions(transactions));
+  }, []);
+
+  //POST FUNCTIONALITY TO DB
+function handleAddTransaction(newTransaction) {
+  setTransactions((prevTransactions) => [...prevTransactions, newTransaction]);
+}
+
+
+  //SEARCH FUNCTIONALITY
+  const transactionsList = transactions.filter((transaction) => {
+    return transaction.description
+      .toLowerCase()
+      .includes(searchParam.toLowerCase());
+  });
+
+  //DELETE FUNCTIONALITY
+  function handleTransactionDelete(deletedTransaction) {
+    const updatedTransactions = transactions.filter(
+      (transaction) => transaction.id !== deletedTransaction.id
+    );
+    setTransactions(updatedTransactions);
+  }
+
+  //FILTER SEARCH OUTPUT DISPLAYED
+  // function filterTransactions() {
+
+  // }
+
+  return (
+    <div>
+      <SearchTransaction
+        searchParam={searchParam}
+        onTransactionSearch={setSearchParam}
+      />
+      <AddTransactionForm onAddTransaction={handleAddTransaction} />
+
+      <table className="ui celled striped padded table">
+        <thead className="ui center aligned header">
+          <tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Amount</th>
+            <th>DELETE?</th>
+          </tr>
+        </thead>
+        {transactionsList.map((transaction) => (
+          <TransactionItem
+            key={transaction.id}
+            transaction={transaction}
+            onDeleteTransaction={handleTransactionDelete}
+          />
+        ))}
+      </table>
+    </div>
+  );
+}
+
+export default TransactionList;
