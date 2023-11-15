9f21c7bc947306fd641c519fca74284c4b9aba4b
dian <dianjeruto@gmail.com>
2023-11-15 20:37:19 +0300

    Updates

---
+++ precache-manifest.1ba8e93132f6cd413f3ed02e5b28d365.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8dac0d95603e773118091f260250e9d",
+    "url": "/Flatiron-App/index.html"
+  },
+  {
+    "revision": "a02cbde1171d21734911",
+    "url": "/Flatiron-App/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/Flatiron-App/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/Flatiron-App/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "a02cbde1171d21734911",
+    "url": "/Flatiron-App/static/js/main.42d36852.chunk.js"
+  },
+  {
+    "revision": "1dc236081165a24ad49d",
+    "url": "/Flatiron-App/static/js/runtime~main.db68bdcf.js"
+  }
+]);
